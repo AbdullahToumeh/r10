42e76c664b2e50d78f52e75803ee40bff8cdeef5
Dakota <dakota.paige@hotmail.com>
2018-05-28 21:05:18 -0700

    Got toggling of conducts working, little bit buggy when changing headers though

--- js/screens/About/AboutConduct.js
+++ js/screens/About/AboutConduct.js
@@ -17,24 +17,47 @@ const conductQuery = gql`
   }
 `;
 
-const AboutConduct = () => {
-  return (
-    <View style={styles.conductContent}>
-      <Query query={conductQuery}>
-        {({ loading, error, data }) => {
-          if (loading) return <LoadingWheel />;
-          if (error) return <Text>Error :(</Text>;
+class AboutConduct extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isVisible: false,
+      currentIndex: -1
+    };
+  }
 
-          return data.allConducts.map(({ description, title }, index) => (
-            <View style={styles.singleConduct} key={index}>
-              <Text style={styles.conductTitle}>+ {title}</Text>
-              <Text>{description}</Text>
-            </View>
-          ));
-        }}
-      </Query>
-    </View>
-  );
-};
+  showContent(index) {
+    this.setState({ isVisible: !this.state.isVisible, currentIndex: index });
+  }
+
+  render() {
+    return (
+      <View style={styles.conductContent}>
+        <Query query={conductQuery}>
+          {({ loading, error, data }) => {
+            if (loading) return <LoadingWheel />;
+            if (error) return <Text>Error :(</Text>;
+
+            return data.allConducts.map(({ description, title }, index) => (
+              <View style={styles.singleConduct} key={index}>
+                <Text
+                  style={styles.conductTitle}
+                  onPress={() => this.showContent(index)}
+                >
+                  + {title}
+                </Text>
+                {this.state.isVisible && this.state.currentIndex === index ? (
+                  <Text id={'conduct' + index}>{description}</Text>
+                ) : (
+                  <Text />
+                )}
+              </View>
+            ));
+          }}
+        </Query>
+      </View>
+    );
+  }
+}
 
 export default AboutConduct;
