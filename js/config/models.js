1ae1ebe40fd8fd2fe006fee7efecf1b5c4866e7f
Dakota <dakota.paige@hotmail.com>
2018-05-30 11:15:40 -0700

    Added realm and started building out faves db

---
+++ js/config/models.js
@@ -0,0 +1,31 @@
+import Realm from 'realm';
+
+const FavesSchema = {
+  name: 'Faves',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    faved_on: 'date'
+  }
+};
+
+const realm = new Realm({ schema: [FavesSchema] });
+
+export default realm;
+
+export const addFave = id => {
+  realm.write(() => {
+    realm.create('Faves', { id: id, faved_on: new Date() });
+  });
+};
+
+export const removeFave = id => {
+  realm.write(() => {
+    const itemToDelete = realm.objects('Faves').filtered('id == $0', id);
+    realm.delete(itemToDelete);
+  });
+};
+
+export const getAllFaves = () => {
+  return realm.objects('Faves');
+};
