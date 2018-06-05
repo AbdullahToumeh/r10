6e77fdfc7ff0e5a7f7e630bb146f9049f21a34e2
Dakota <dakota.paige@hotmail.com>
2018-06-05 11:03:25 -0700

    Fixed the fucking maps

--- ios/R10/AppDelegate.m
+++ ios/R10/AppDelegate.m
@@ -9,13 +9,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import GoogleMaps; //add this line if you want to use Google Maps
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyB4ssSLLcikHR_DPkU5kD8QdGZ-HuyjHf8"]
 
   NSURL *jsCodeLocation;
 
